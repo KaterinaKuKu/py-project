5cc91bf9914d19d8580191fc2f3dec0273d5d8b0
kater <ekaterina.kudrya29@gmail.com>
2024-03-04 15:40:43 +0400

    commit

--- py-project/funcs.py
+++ py-project/funcs.py
@@ -1,5 +1,5 @@
 def add_numbers(a, b):
     return a + b
 
-def add_numbers(c, d):
+def add_numbers(a, b):
     return b - a
---
+++ py-project/main.py
@@ -0,0 +1,10 @@
+from funcs import add_numbers
+
+num1 = 5
+num2 = 7
+
+result = add_numbers(num1, num2)
+
+print(f"Результат сложения {num1} и {num2}: {result}")
+
+print(f"Результат вычитания {num1} и {num2}: {result}")
